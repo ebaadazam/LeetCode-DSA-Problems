ac22cc4a5b7b9c981caddde80983f41aff43441a
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2023-11-20 23:55:31 +0530

    Add files via upload

---
+++ Q28_Container-with_most_water.py
@@ -0,0 +1,48 @@
+'''
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the
+ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container.
+
+Example 1:
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+Input: height = [1,1]
+Output: 1
+'''
+
+# One Approach (Efficient)
+def container(height):
+    result = 0
+    l, r = 0, len(height)-1
+    while l < r:
+        area = (r - l) * min(height[l], height[r])
+        result = max(area, result)
+
+        if height[l] < height[r]:
+            l += 1
+        elif height[r] < height[l]:
+            r -= 1
+        else: # If l == r either increase left pointer or decrease the right pointer
+            l += 1
+    return result
+
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+print(container(height))
+
+
+# Another Approach (Time Limit Exceeded)
+def container(height):
+    result = 0
+    for l in range(len(height)):
+        for r in range(l+1, len(height)):
+            area = (r-l) * min(height[l], height[r])
+            result = max(area, result)
+    return result
+
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+print(container(height))
